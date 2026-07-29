b0b98d062caaf639db8ba8b79e04414f5fd427b5
sethellphillip <sethellphillip@gmail.com>
2026-07-29 00:16:08 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -48,6 +48,10 @@ const studentProfile = {
     {
       title: "Exploring TCGA Breast Cancer Clinical Data",
       url: "https://rpubs.com/sethphillipsp/1447207"
+    },
+    {
+      title: "Heatmaps with TCGA Breast Cancer Gene Expression Data",
+      url: "https://rpubs.com/sethphillipsp/1449902"
     }
   ],
 
